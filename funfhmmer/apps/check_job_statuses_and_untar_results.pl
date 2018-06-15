2edf7d9e3de7bd5949ba087db0e5a59ca9323733
Sayoni Das <sayoni.das.12@ucl.ac.uk>
2018-06-15 12:20:10 +0100

    Update check jobs script

--- funfhmmer/apps/check_job_statuses_and_untar_results.pl
+++ funfhmmer/apps/check_job_statuses_and_untar_results.pl
@@ -5,7 +5,42 @@ RESULTS=${PROJECTHOME}/results/
 JOB_STATUS=${PROJECTHOME}/job_status/
 #SCRIPTNAME="run_funfhmmer_in_bchuckle_cluster-SFs."
 
-# Check whether the jobs are all completed
+echo ""
+echo "Untarring the result folders:"
+echo ""
+
+# Untar result directories and look for empty files
+for file in `find $RESULTS -name "*.tar.gz"`
+do
+	SF=`basename $file .tar.gz`
+	echo "Checking $SF results:"
+	tar -xvzf $file -C $RESULTS
+  for f in $RESULTS/$SF/* $RESULTS/$SF/funfam_alignments/* $RESULTS/$SF/funfam_alignments/analysis_data/* ;
+  do
+    if [ -s $f ] ; then
+      x=$f #dummy line, does nothing
+    else
+      echo " - ERROR: $f is EMPTY!"
+    fi
+  done
+  logfile=$RESULTS/$SF/$SF.LOG
+  if [ -s $logfile ] ; then
+    loglastline=`tail -n 1 $logfile`
+    array1=($loglastline)
+    check_lastline=${array1[-1]}
+    hours=${array1[-2]}
+    #echo "$array1, $check_lastline, $hours"
+    if [ $check_lastline == "hours" ] ; then
+      echo " - $SF was completed in $hours hours"
+    fi
+  fi
+done
+
+echo ""
+echo "Here are the job status files:"
+echo ""
+
+# Check whether the jobstatus have any errors
 for file in `find $JOB_STATUS -name "*.stdout"`
 do
   filename=`basename $file .stdout`
@@ -13,19 +48,10 @@ do
   searchline1=`grep "#JOB COMPLETE for " $file`
   array1=${searchline1}
   SF=${array1[-1]}
-  searchline2=`grep "hours" $file`
-  array2=($searchline2)
-  hours=${array2[-2]}
-  echo "$filename: $SF completed in $hours hours"
-done
-
-# Untar result directories
-for file in `find $RESULTS -name "*.tar.gz"`
-do
-	sf=`basename $file .tar.gz`
-	echo $sf
-	mkdir $DIR/$sf
-	echo "$DIR/$sf"
-	echo $file
-	tar -xvzf $file -C $DIR/$sf
+  searchline2=`grep -i "error" $file`
+  if [ -z "$searchline2" ]; then
+    echo "$filename: $SF has no errors."
+  else
+    echo "$filename: $SF has ERRORS!"
+  fi
 done
