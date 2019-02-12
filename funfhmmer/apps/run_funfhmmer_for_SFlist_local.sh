32eb462727ed5d6cc2b8906dd8fb0534f8b49c3e
Sayoni Das <sayoni.das.12@ucl.ac.uk>
2019-02-12 16:12:37 +0000

    Remove default project name

--- funfhmmer/apps/run_funfhmmer_for_SFlist_local.sh
+++ funfhmmer/apps/run_funfhmmer_for_SFlist_local.sh
@@ -3,7 +3,9 @@
 if [ "$#" -lt 2 ]; then
 	echo "Usage:"
     echo ""
-    echo "$0 <PROJECTHOME> <SFLISTFILE> <OPTIONAL: AUTOMERGE_LOWEVAL> <OPTIONAL: PROJECTHOME_NAME>"
+    echo "$0 <PROJECTHOME> <SFLISTFILE> <OPTIONAL: AUTOMERGE_LOWEVAL (default:1)>"
+    echo ""
+    echo "Use AUTOMERGE_LOWEVAL=1 for 1st iteration of FunFHMMer and for other iterations, use 0"
     echo ""
 	echo "ERROR: All required arguments has not been passed, received $# arguments"
 	exit;
@@ -13,8 +15,6 @@ DIR=$1
 SFLISTFILE=$2
 AUTOMERGE_LOWEVAL=$3
 AUTOMERGE_LOWEVAL=${AUTOMERGE_LOWEVAL:-1}
-PROJECT_NAME=$4
-PROJECT_NAME=${PROJECT_NAME:-funfhmmer_v4_2_0}
 
 # Get the project directory
 if [ ! -d "$DIR" ]; then
@@ -29,7 +29,7 @@ function print_date {
 
 REMOTE_USER=`whoami`
 
-print_date "REMOTE_USER         $REMOTE_USER"
+print_date "USER                $REMOTE_USER"
 print_date "FUNFHMMER_DIR       $DIR"
 print_date "SFLISTFILE          $SFLISTFILE"
 print_date "AUTOMERGE_LOWEVAL   $AUTOMERGE_LOWEVAL"
@@ -39,25 +39,34 @@ APPSDIR=$DIR/apps
 RESULTSDIR=$DIR/results
 REDO_SUPS=$RESULTSDIR/REDO_SUPS.list
 
-REMOTE_DATA_PATH=/scratch/scratch/${REMOTE_USER}/${PROJECT_NAME}
-LOCAL_TMP_DIR=${REMOTE_DATA_PATH}/results/local_run
+LOCAL_TMP_DIR=$DIR/results/local_run
 
 mkdir -p -v $LOCAL_TMP_DIR
 
-cat $SFLISTFILE | while read superfamily
+cat $SFLISTFILE | while read superfamilyline
 do
- 	if [ -d $DATADIR/$superfamily/ ]; then
+	
+	stringarray=($superfamilyline)
+	superfamily=${stringarray[0]}
+	GEMMADIR=${stringarray[1]}
+
+	SF_DATA_DIR=$DATADIR/$superfamily
+
+	#echo "$SF_DATA_DIR"
+	
+ 	if [ -d $SF_DATA_DIR ]; then
         
-        echo ""
-        echo "Generating FunFams locally for $superfamily here: $LOCAL_TMP_DIR/"
-        echo ""
+	        echo ""
+        	echo "Generating FunFams locally for $superfamily here: $LOCAL_TMP_DIR/ ..."
+        	echo ""
         
-        rsync -raz $DATADIR/$superfamily/ $LOCAL_TMP_DIR/$superfamily/
-        rsync -raz $DATADIR/$superfamily/starting_cluster_alignments/ $LOCAL_TMP_DIR/$superfamily/funfam_alignments/
+        	rsync -raz $SF_DATA_DIR/ $LOCAL_TMP_DIR/$superfamily/
+        	rsync -raz $SF_DATA_DIR/starting_cluster_alignments/ $LOCAL_TMP_DIR/$superfamily/funfam_alignments/
         
-        perl $APPSDIR/funfhmmer.pl --sup $superfamily --dir $LOCAL_TMP_DIR/$superfamily --groupsim_matrix id --automerge_loweval $AUTOMERGE_LOWEVAL 1> $LOCAL_TMP_DIR/$superfamily/funfhmmer.stdout 2> $LOCAL_TMP_DIR/$superfamily/funfhmmer.stderr
+        	perl $APPSDIR/funfhmmer.pl --sup $superfamily --dir $LOCAL_TMP_DIR/$superfamily --groupsim_matrix id --automerge_loweval $AUTOMERGE_LOWEVAL 1> $LOCAL_TMP_DIR/$superfamily/funfhmmer.stdout 2> $LOCAL_TMP_DIR/$superfamily/funfhmmer.stderr
         
-        echo "Finished generating FunFams for $superfamily."
-        
-    fi
+        	echo "Finished generating FunFams for $superfamily."
+            echo ""
+	
+    	fi
 done
