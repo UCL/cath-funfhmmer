d1386e462076a362b98a1a2f933f9fa7d93e9213
Sayoni Das <sayoni.das.12@ucl.ac.uk>
2019-04-26 16:43:26 +0100

    Pass named arguments

--- funfhmmer/apps/run_funfhmmer_for_SFlist_hpc.sh
+++ funfhmmer/apps/run_funfhmmer_for_SFlist_hpc.sh
@@ -15,44 +15,65 @@
 #set -x
 
 if [ "$#" -lt 5 ]; then
-    echo "USAGE:"
+	echo "USAGE:"
     echo ""
-    echo "bash $0 <PROJECTHOME> <SFLISTFILE> <HPC_CLUSTER> <PROJECTHOME_NAME> <AUTOMERGE_LOWEVAL> <OPTIONAL: TRACEDIR (default:PROJECTHOME/data)> <OPTIONAL: FFDIR (default:PROJECTHOME/results)>"
+    echo "bash $0 --projecthome=<PROJECTHOME> --projectlist=<PROJECT_LIST> --hpc=<HPC_CLUSTER (myriad/legion/chuckle)> --projectname=<PROJECTNAME> --automerge=<0/1>"
+    echo ""
+    echo "Optional:"
+    echo "         --tracedir=<TREEDIR (default: 2nd col. of PROJECT_LIST)>"
+    echo "         --ffdir=<FF_DIR (default:PROJECTHOME/results)>"
+    echo ""
+    echo "NOTE: Use AUTOMERGE_LOWEVAL=1 for 1st iteration of FunFHMMer and for other iterations, use 0"
     echo ""
 	echo "ERROR: All required arguments has not been passed, received $# arguments"
     echo ""
 	exit;
 fi
 
-DIR=$1
+for i in "$@"
+do
+    case $i in
+        --projecthome=*)
+        DIR="${i#*=}"
+        shift
+        ;;
+        --projectlist=*)
+        SFLISTFILE="${i#*=}"
+        shift
+        ;;
+        --hpc=*)
+        HPC_CLUSTER="${i#*=}"
+        shift
+        ;;
+        --projectname=*)
+        PROJECTNAME="${i#*=}"
+        shift
+        ;;
+        --automerge=*)
+        AUTOMERGE_LOWEVAL="${i#*=}"
+        shift
+        ;;
+        --tracedir=*)
+        TRACEDIR="${i#*=}"
+        shift
+        ;;
+        --ffdir=*)
+        FF_DIR="${i#*=}"
+        shift
+        ;;
+        *)
+        
+        ;;
+    esac
+done
 
-SFLISTFILE=$2
-
-HPC_CLUSTER=$3
-
-PROJECT_NAME=$4
-
-AUTOMERGE_LOWEVAL=$5
-#AUTOMERGE_LOWEVAL=${AUTOMERGE_LOWEVAL:-1}
-
-TRACEDIR=$6
-TRACEDIR=${TRACEDIR:-$DIR/data}
-
-FFDIR=$7
+# use default values of FF_DIR is nothing is provided
 FFDIR=${FFDIR:-$DIR/results}
-
 
 # Get the project directory
 if [ ! -d "$DIR" ]; then
         echo ""
         echo "ERROR: '$DIR' does not exist for $HOSTNAME $SGE_O_HOST ."
-        echo ""
-        exit;
-fi
-
-if [ ! -d "$TRACEDIR" ]; then
-        echo ""
-        echo "ERROR: '$TRACEDIR' does not exist for $HOSTNAME $SGE_O_HOST ."
         echo ""
         exit;
 fi
@@ -76,22 +97,36 @@ REMOTE_USER=`whoami`
 
 print_date "REMOTE_USER         $REMOTE_USER"
 print_date "RUN_ENV             $HPC_CLUSTER"
-print_date "FUNFHMMER_DIR       $DIR"          
+print_date "FUNFHMMER_DIR       $DIR"
 print_date "SFLIST              $SFLISTFILE"
-print_date "TRACEDIR            $TRACEDIR"
 print_date "FFDIR               $FFDIR"
 print_date "AUTOMERGE_LOWEVAL   $AUTOMERGE_LOWEVAL"
+
+if [ -z "$TRACEDIR" ]
+then
+    print_date "TRACEDIR                    2nd col. in $SFLISTFILE"
+else
+    print_date "TRACEDIR                    $TRACEDIR"
+fi
 
 APPSDIR=$DIR/apps
 REDO_SUPS=$FFDIR/REDO_SUPS.$JOB_ID.list
 
 superfamily=$(cat $SFLISTFILE | head -n $SGE_TASK_ID | tail -n 1 | awk '{printf $1}')
+GEMMADIR=$(cat $SFLISTFILE | head -n $SGE_TASK_ID | tail -n 1 | awk '{printf $2}')
 
 if [ -z "$superfamily" ]; then
     echo ""
     echo "ERROR: superfamily name is empty!"
     echo ""
     exit;
+fi
+
+if [ -z "$TRACEDIR" ]
+then
+    SF_DATA_DIR=$GEMMADIR
+else
+    SF_DATA_DIR=$TRACEDIR/$superfamily/simple_ordering.hhconsensus.windowed
 fi
 
 time=$(date)
@@ -101,7 +136,7 @@ echo "[$time] #Processing  ${superfamily} .."
 echo ""
 
 run_hpc () {
-
+    
     # create working temp dir
     mkdir -p -v $LOCAL_TMP_DIR
     cd $LOCAL_TMP_DIR
@@ -112,9 +147,9 @@ run_hpc () {
     echo "[$time] #Start copying starting clusters of ${superfamily}.."
 
     #Copy the folder to FFer working dir
-    rsync -raz $TRACEDIR/$superfamily/ $LOCAL_TMP_DIR/$superfamily/
+    rsync -raz $SF_DATA_DIR/ $LOCAL_TMP_DIR/$superfamily/
 
-    rsync -raz $TRACEDIR/$superfamily/starting_cluster_alignments/ $LOCAL_TMP_DIR/$superfamily/funfam_alignments/
+    rsync -raz $SF_DATA_DIR/starting_cluster_alignments/ $LOCAL_TMP_DIR/$superfamily/funfam_alignments/
 
     time=$(date)
     echo "[$time] #---DONE"
@@ -123,21 +158,21 @@ run_hpc () {
     time=$(date)
     echo "[$time] #Start generating FunFams"
 
-    perl $APPSDIR/funfhmmer.pl --sup $superfamily --dir $LOCAL_TMP_DIR/$superfamily --groupsim_matrix id 1 --automerge_loweval $AUTOMERGE_LOWEVAL > $LOCAL_TMP_DIR/$superfamily/funfhmmer.stdout 2> $LOCAL_TMP_DIR/$superfamily/funfhmmer.stderr   
+    perl $APPSDIR/funfhmmer.pl --sup $superfamily --dir $LOCAL_TMP_DIR/$superfamily --groupsim_matrix id 1 --automerge_loweval $AUTOMERGE_LOWEVAL > $LOCAL_TMP_DIR/$superfamily/funfhmmer.stdout 2> $LOCAL_TMP_DIR/$superfamily/funfhmmer.stderr
 
-            
+
     # For using --groupsim_matrix mc, that uses Mclaclan matrix,1972 the scoreanalysis subroutine in GSanalysis needs to be written for it. For this, the threshold values of SDP prediction by Groupsim needs to be determined using correlation by plotting groupsim scores calculated using Identity matrix and McLaclan matrix and determing an equivalent threshold.
 
     SUP_RUN_LOG=$LOCAL_TMP_DIR/$superfamily/$superfamily.LOG
 
     if grep -Fwq "hours" $SUP_RUN_LOG; then
-    
+
         time=$(date)
         echo "[$time] #Finished generating FunFams.."
-    
+
         rm -r $LOCAL_TMP_DIR/$superfamily/merge_node_alignments/
         rm -r $LOCAL_TMP_DIR/$superfamily/starting_cluster_alignments/
-	
+
         echo ""
         time=$(date)
         echo "[$time] #Copying back generated FunFams for ${superfamily}.."
@@ -152,15 +187,15 @@ run_hpc () {
         time=$(date)
         echo "[$time] #JOB COMPLETE for ${superfamily}."
         echo ""
-    
+
     else
-    
+
         echo
         time=$(date)
         echo "[$time] #FunFam generation was NOT complete for ${superfamily}"
         echo $superfamily >> $REDO_SUPS
         echo ""
-    
+
     fi
 }
 
@@ -168,20 +203,20 @@ case "$HPC_CLUSTER" in
 
 # on chuckle cluster
 chuckle)
-    
+
     	SCRATCH_DIR=/scratch0/${REMOTE_USER}-funfhmmer
     	LOCAL_TMP_DIR=$SCRATCH_DIR/${JOB_ID}_${SGE_TASK_ID}
-    
+
         run_hpc
         ;;
-    
+
 # on legion cluster
 legion)
         # path to gemma data in legion scratch dir
 
         REMOTE_DATA_PATH=/scratch/scratch/${REMOTE_USER}/${PROJECT_NAME}
         LOCAL_TMP_DIR=${REMOTE_DATA_PATH}/results/${JOB_ID}_${SGE_TASK_ID}
-    
+
         run_hpc
         ;;
 
@@ -190,7 +225,7 @@ myriad)
 
         REMOTE_DATA_PATH=/scratch/scratch/${REMOTE_USER}/${PROJECT_NAME}
     	LOCAL_TMP_DIR=${REMOTE_DATA_PATH}/results/${JOB_ID}_${SGE_TASK_ID}
-    
+
         run_hpc
         ;;
 
