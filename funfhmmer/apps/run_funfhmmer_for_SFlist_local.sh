206385fe0ee2aaa876327a2130ce7beef47ef23e
Sayoni Das <sayoni.das.12@ucl.ac.uk>
2019-04-26 13:45:42 +0100

    Pass named arguments to script

--- funfhmmer/apps/run_funfhmmer_for_SFlist_local.sh
+++ funfhmmer/apps/run_funfhmmer_for_SFlist_local.sh
@@ -3,27 +3,51 @@
 if [ "$#" -lt 3 ]; then
 	echo "USAGE:"
     echo ""
-    echo "bash $0 <PROJECTHOME> <SFLISTFILE> <AUTOMERGE_LOWEVAL> <OPTIONAL: TRACEDIR (default:PROJECTHOME/data)> <OPTIONAL: LOCAL_FF_DIR (default:TRACEDIR/local_run)>"
+    echo "bash $0 --projecthome=<PROJECTHOME> --projectlist=<PROJECT_LIST> --automerge=<0/1>"
     echo ""
-    echo "      NOTE: Use AUTOMERGE_LOWEVAL=1 for 1st iteration of FunFHMMer and for other iterations, use 0"
+    echo "Optional:"
+    echo "         --tracedir=<TREEDIR (default:2nd col. of PROJECT_LIST)>"
+    echo "         --ffdir=<LOCAL_FF_DIR (default:PROJECTHOME/local_run)>"
+    echo ""
+    echo "NOTE: Use AUTOMERGE_LOWEVAL=1 for 1st iteration of FunFHMMer and for other iterations, use 0"
     echo ""
 	echo "ERROR: All required arguments has not been passed, received $# arguments"
+    echo ""
 	exit;
 fi
 
-DIR=$1
+for i in "$@"
+do
+    case $i in
+        --projecthome=*)
+        DIR="${i#*=}"
+        shift
+        ;;
+        --projectlist=*)
+        SFLISTFILE="${i#*=}"
+        shift
+        ;;
+        --automerge=*)
+        AUTOMERGE_LOWEVAL="${i#*=}"
+        shift
+        ;;
+        --tracedir=*)
+        TRACEDIR="${i#*=}"
+        shift
+        ;;
+        --ffdir=*)
+        LOCAL_FF_DIR="${i#*=}"
+        shift
+        ;;
+        *)
+        
+        ;;
+    esac
+done
 
-SFLISTFILE=$2
+# use default values of LOCAL_FF_DIR if nothing is provided
 
-AUTOMERGE_LOWEVAL=$3
-#AUTOMERGE_LOWEVAL=${AUTOMERGE_LOWEVAL:-1}
-
-TRACEDIR=$4
-TRACEDIR=${TRACEDIR:-$DIR/data}
-
-LOCAL_FF_DIR=$5
-LOCAL_FF_DIR=${LOCAL_FF_DIR:-$TRACEDIR/local_run}
-
+LOCAL_FF_DIR=${LOCAL_FF_DIR:-$PROJECTHOME/local_run}
 
 # Get the project directory
 if [ ! -d "$DIR" ]; then
@@ -32,14 +56,6 @@ if [ ! -d "$DIR" ]; then
 fi
 
 # Check RESULTDIR directory
-
-if [ ! -d "$TRACEDIR" ]; then
-        echo ""
-        echo "'$TRACEDIR' does not exist."
-        mkdir -p $TRACEDIR || die "mkdir failed for $TRACEDIR"
-        echo ""
-        exit;
-fi
 
 if [ ! -d "$LOCAL_FF_DIR" ]; then
         echo ""
@@ -60,44 +76,52 @@ REMOTE_USER=`whoami`
 print_date "USER                        $REMOTE_USER"
 print_date "FUNFHMMER_DIR               $DIR"
 print_date "PROJECT_LIST_FILE           $SFLISTFILE"
-print_date "TRACEDIR                    $TRACEDIR"
 print_date "LOCAL_FF_DIR                $LOCAL_FF_DIR"
 print_date "AUTOMERGE_LOWEVAL           $AUTOMERGE_LOWEVAL"
 
+if [ -z "$TRACEDIR" ]
+then
+    print_date "TRACEDIR                    2nd col. in $SFLISTFILE"
+else
+    print_date "TRACEDIR                    $TRACEDIR"
+fi
 
 APPSDIR=$DIR/apps
 REDO_SUPS=$TRACEDIR/REDO_SUPS.list
 
 cat $SFLISTFILE | while read superfamilyline
 do
-	
+
 	stringarray=($superfamilyline)
 	superfamily=${stringarray[0]}
 	GEMMADIR=${stringarray[1]}
-    
-    #echo "$GEMMADIR"
 
-	SF_DATA_DIR=$TRACEDIR/$superfamily
-	
+    if [ -z "$TRACEDIR" ]
+    then
+        SF_DATA_DIR=$GEMMADIR
+    else
+        SF_DATA_DIR=$TRACEDIR/$superfamily/simple_ordering.hhconsensus.windowed
+    fi
+
  	if [ -d $SF_DATA_DIR ]; then
-        
+
 	        echo ""
         	echo "Generating FunFams locally for $superfamily:"
         	echo ""
-            
+
             echo "-- PROJECT_DATA_DIR            $SF_DATA_DIR"
             echo "-- PROJECT_RESULT_DIR          $LOCAL_FF_DIR/$superfamily/"
-        
+
         	rsync -raz $SF_DATA_DIR/ $LOCAL_FF_DIR/$superfamily/
         	rsync -raz $SF_DATA_DIR/starting_cluster_alignments/ $LOCAL_FF_DIR/$superfamily/funfam_alignments/
-        
+
         	perl $APPSDIR/funfhmmer.pl --sup $superfamily --dir $LOCAL_FF_DIR/$superfamily --groupsim_matrix id --automerge_loweval $AUTOMERGE_LOWEVAL 1> $LOCAL_FF_DIR/$superfamily/funfhmmer.stdout 2> $LOCAL_FF_DIR/$superfamily/funfhmmer.stderr
-        
+
         	echo "-- Finished generating FunFams for $superfamily."
             echo ""
             echo ""
-            
+
             #exit 0;
-	
+
     	fi
 done
